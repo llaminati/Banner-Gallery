070d2aa627d9d601f1e0f7cfd2b9d2ac70528085
alvinlo92 <alvinlo92@yahoo.com>
2019-08-23 18:07:25 -0700

    Create API endpoint to read table data
    Move seed code into seperate seed.js file
    Change index.js to only select/read tables

--- db/index.js
+++ db/index.js
@@ -1,37 +1,24 @@
 const mysql = require('mysql');
 const mysqlConfig = require('./config.js');
-var faker = require('faker');
+const faker = require('faker');
 
 const connection = mysql.createConnection(mysqlConfig);
 
-const seedListing = () => {
-  for (var i = 1; i <= 100; i++) {
-    connection.query('INSERT INTO listing (id) VALUE (?)', [i], (error, results) => {
-      if (error) {
-        console.log(error, 'error seed listing');
-      }
-    }
-  )};
-}
+const getListings = (cb) => {
+  connection.query('SELECT * FROM listing', (error, results) => {
+    if (error) return error;
+    cb(null, results);
+  })
+};
 
-const seedImages = () => {
-  for (var i = 1; i <= 100; i++) {
-    let randomListing = Math.floor(Math.random() * 100);
-    let randomNum = faker.random.number() + '';
-    let num = Number(randomNum[0]);
-
-    connection.query('INSERT INTO images (url, description, user_submit, date, unrelated_flag, inappropriate_flag, dislike_flag, listing_id) VALUE (?, ?, ?, ?, ?, ?, ?, ?)', [faker.image.imageUrl(), faker.random.words(), faker.random.boolean(), faker.date.past(), num, num, num, randomListing], (error, results) => {
-      if (error) {
-        console.log(error, 'error seed images');
-      }
-    })
-  }
-}
-
-seedListing();
-seedImages();
+const getImages = (cb) => {
+  connection.query('SELECT * FROM images', (error, results) => {
+    if (error) return error;
+    cb(null, results);
+  })
+};
 
 module.exports = {
-  seedListing,
-  seedImages
+  getListings,
+  getImages
 }
--- server/index.js
+++ server/index.js
@@ -1,5 +1,21 @@
-concst express require('express');
+const express = require('express');
+const db = require('../db/index.js');
+
 const app = express();
 const port = 3000;
+
+app.use(express.static(__dirname + 'public'));
+
+app.get('/api/L1/banner/images', (req, res) => {
+  // db.getListings((error, listing) => {
+  //   if (error) return error;
+  //   res.send(listing);
+  // });
+
+  db.getImages((error, images) => {
+    if (error) return error;
+    res.send(images);
+  });
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
