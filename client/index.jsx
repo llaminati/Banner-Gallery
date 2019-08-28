6de115df3ac51e49df33d3518e35f7228e0fe56f
alvinlo92 <alvinlo92@yahoo.com>
2019-08-27 17:05:53 -0700

    Commit rest of files

---
+++ client/index.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+import styled from 'styled-components';
+
+import Image from './components/Image.jsx';
+
+const Gallery = styled.div`
+  width: 1600px;
+  height: 288px;
+  display: flex;
+  justify-content: center;
+  background-color: #2d333f;
+`;
+
+class BannerGallery extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      images: []
+    };
+  }
+
+  componentDidMount() {
+    axios.get('/api/L1/banner/images')
+      .then((res) => {
+        this.setState({
+          images: res.data
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  render() {
+    return (
+      <Gallery>
+        {this.state.images.map((image, i) => <Image key={i} image={image}/>)}
+      </Gallery>
+    );
+  }
+}
+
+ReactDOM.render(<BannerGallery />, document.getElementById('app'));
--- db/seed.js
+++ db/seed.js
@@ -4,9 +4,9 @@ const faker = require('faker');
 
 const connection = mysql.createConnection(mysqlConfig);
 
-const seedListing = () => {
+const seedListings = () => {
   for (var i = 1; i <= 100; i++) {
-    connection.query('INSERT INTO listing (listing) VALUE (?)', [`L${i}`], (error, results) => {
+    connection.query('INSERT INTO listings (listings) VALUE (?)', [`L${i}`], (error, results) => {
       if (error) { console.log(error); }
     });
   }
@@ -14,26 +14,32 @@ const seedListing = () => {
 
 const seedImages = () => {
   for (var i = 1; i <= 100; i++) {
-    let randomUnrelatedFlag = Math.floor(Math.random() * 3);
-    let randomInappropriate = Math.floor(Math.random() * 3);
-    let randomDislike = Math.floor(Math.random() * 3);
-    let randomListing = Math.floor(Math.random() * 100);
+    let imageUrl = `https://llaminati-images.s3-us-west-1.amazonaws.com/${i}.jpg`;
 
-    // prevents listing from being 0
-    if (randomListing === 0) {
-      randomListing++;
-    }
-
-    connection.query('INSERT INTO images (url, description, user_submit, date, unrelated_flag, inappropriate_flag, dislike_flag, listing_id) VALUE (?, ?, ?, ?, ?, ?, ?, ?)', [faker.image.imageUrl(), faker.random.words(), faker.random.boolean(), faker.date.past(), randomUnrelatedFlag, randomInappropriate, randomDislike, randomListing], (error, results) => {
+    connection.query('INSERT INTO images (url, description, user_submit, date, unrelated_flag, inappropriate_flag, dislike_flag) VALUE (?, ?, ?, ?, ?, ?, ?)', [imageUrl, faker.lorem.words(), faker.random.boolean(), faker.date.past(), 0, 0, 0], (error, results) => {
       if (error) { console.log(error); }
     });
   }
 };
 
-seedListing();
-seedImages();
+const seedListingsImages = () => {
+  let amountOfImages = 0;
 
-module.exports = {
-  seedListing,
-  seedImages
+  while (amountOfImages < 1000) {
+    let randListingId = Math.floor(Math.random() * 100);
+    let randImageId = Math.floor(Math.random() * 100);
+
+    if (randListingId === 0) { randListingId++; }
+    if (randImageId === 0) { randImageId++; }
+
+    connection.query('INSERT INTO listings_images (listing_id, image_id) VALUE (?, ?)', [randListingId, randImageId], (error, results) => {
+      if (error) { console.log(error); }
+    });
+
+    amountOfImages++;
+  }
 };
+
+seedListings();
+seedImages();
+seedListingsImages();
