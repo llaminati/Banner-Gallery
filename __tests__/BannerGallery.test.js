4a11de92e1560082896fbd4c9562c2fccda886c1
alvinlo92 <alvinlo92@yahoo.com>
2019-08-28 21:33:40 -0700

    Add more test for BannerGallery
    Configure circleci

--- __tests__/BannerGallery.test.js
+++ __tests__/BannerGallery.test.js
@@ -5,36 +5,41 @@ import Adapter from 'enzyme-adapter-react-16';
 
 import BannerGallery from '../client/components/BannerGallery.jsx';
 
-jest.mock('axios', () => {
-  const imageData = [
-    {
-      'id': 34,
-      'url': 'https://llaminati-images.s3-us-west-1.amazonaws.com/14.jpg',
-      'description': 'quidem repellat magni',
-      'user_submit': 0,
-      'date': '2019-04-15T07:00:00.000Z',
-      'unrelated_flag': 0,
-      'inappropriate_flag': 0,
-      'dislike_flag': 0,
-      'listing_id': 1,
-      'image_id': 14
-    },
-  ];
+jest.mock('axios');
 
-  return {
-    get: jest.fn(() => Promise.resolve({ data: imageData })),
-  };
-});
+const imageData = [
+  {
+    'id': 34,
+    'url': 'https://llaminati-images.s3-us-west-1.amazonaws.com/14.jpg',
+    'description': 'quidem repellat magni',
+    'user_submit': 0,
+    'date': '2019-04-15T07:00:00.000Z',
+    'unrelated_flag': 0,
+    'inappropriate_flag': 0,
+    'dislike_flag': 0,
+    'listing_id': 1,
+    'image_id': 14
+  },
+];
 
 describe('BannerGallery component', () => {
   test('renders without issue', () => {
+    axios.get.mockImplementation(() => Promise.resolve({ data: imageData }));
     const wrapper = shallow(<BannerGallery />);
     expect(wrapper.exists()).toBe(true);
   });
 
   it('should fetch a list of images', () => {
-    const getSucess = jest.spyOn(axios, 'get');
+    axios.get.mockImplementation(() => Promise.resolve({ data: imageData }));
+    const getSuccess = jest.spyOn(axios, 'get');
     const wrapper = shallow(<BannerGallery />);
-    expect(getSucess).toBeCalled();
+    expect(getSuccess).toBeCalled();
+  });
+
+  it('should error when fetch', () => {
+    axios.get.mockImplementation(() => Promise.reject());
+    const getError = jest.spyOn(axios, 'get');
+    const wrapper = shallow(<BannerGallery />);
+    expect(getError).toBeCalled();
   });
 });
